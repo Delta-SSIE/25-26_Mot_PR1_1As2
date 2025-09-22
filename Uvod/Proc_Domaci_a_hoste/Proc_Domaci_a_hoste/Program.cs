e8403ed87dc1254d21145989436d881d389b31d0
Jan Mottl <mottja@delta-skola.cz>
2025-09-22 10:54:34 +0200

    kk

---
+++ Uvod/Proc_Domaci_a_hoste/Proc_Domaci_a_hoste/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Proc_Domaci_a_hoste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej skore domácích: ");
+            string domaci = Console.ReadLine();
+
+            Console.Write("Zadej skore hostů: ");
+            string hoste = Console.ReadLine();
+
+            Console.WriteLine("Výsledek zápasu je " + domaci + ":" +hoste);
+
+            //Výpis do konzole bez použití proměnná + string
+
+            //Výpis pomocí placeholderů a $ (na začátku musí být)
+            Console.WriteLine($"Výsledek zápasu je {domaci}:{hoste}");
+            //složené závorky ({}) ALTGR + B a ALTGR + N
+            //dollar ($) se píše ALTGR+ů
+
+            //Výpis pomocí pořadových čísel
+            Console.WriteLine("Výsledek zápasu je {0}:{1}", domaci,hoste);
+            //0 - domaci
+            //1 - hoste
+            //pořadová čísla jsou od 0, první za "" je proměnná, která jde na místo {0}
+
+        }
+    }
+}
