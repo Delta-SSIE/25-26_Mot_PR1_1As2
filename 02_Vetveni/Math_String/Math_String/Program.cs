5eaf2bf89847daa1539a8103087ac4eabf22eb72
Jan Mottl <mottja@delta-skola.cz>
2025-10-13 11:40:13 +0200

    kk

---
+++ 02_Vetveni/Math_String/Math_String/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace Math_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MATH - matematické funkce/metody
+
+            Console.WriteLine(Math.Max(5,8)); //metoda Max vrací větší ze dvou čísel
+            Console.WriteLine(Math.Min(5, 8)); //metoda Min vrací menší ze dvou čísel
+
+            Console.WriteLine(Math.Sqrt(24)); //Sqrt vrací druhou odmocninu ze zadaného čísla
+            Console.WriteLine(Math.Abs(-22)); //Abs vrací absolutní hodnotu daného čísla
+
+            Console.WriteLine(Math.PI); //vrací PI hodnotu (v doublu)
+            Console.WriteLine(Math.Pow(2,5)); //mocnění - první číslo je to co bude mocněné, druhé čím se bude mocnit
+            //příklad je 2 na 5tou
+
+            //String 
+            string txt = "Ahoj";
+
+            //Délka textu
+            //nazev_promenne.Length vrací délku daného řetězce
+            Console.WriteLine(txt.Length);
+
+            //Indexace stringu (práce s převodem na char)
+            //Hranaté závorky = Pravý alt + F = [; Pravý alt + G = ]
+            //hodnota v proměnné txt (string) na 0. indexu
+            //txt = "Ahoj"
+            //0. index = A
+            //3. index = j
+            Console.WriteLine(txt[0]);
+
+            char prvni_pismeno = txt[0]; //vratí znak na 0. indexu
+            Console.WriteLine(prvni_pismeno);
+            char treti_pismeno = txt[3]; //vratí znak na 3. indexu
+            Console.WriteLine(treti_pismeno);
+            char posledni_pismeno = txt[txt.Length-1]; //vratí to znak na indexu, který se rovná délce řetězce -1
+            //v našem případě je délka 4, tzn. 4-1=3
+            Console.WriteLine(posledni_pismeno);
+
+            //vrácení indexu znaku
+            Console.WriteLine(txt.IndexOf('o')); //vrátí první index prvního výskytu znaku 'o' v rámci řetězce txt
+            Console.WriteLine(txt.LastIndexOf('o')); //vrací poslední výskyt znaku 'o' v txt
+
+            string slovo = "folklor";
+            Console.WriteLine(slovo.IndexOf('o')); //první výskyt = 1
+            Console.WriteLine(slovo.LastIndexOf('o')); //poslední výskyt = 5
+
+            //Uživatel zadá znak a program mu řekne, jestli to je malé písmeno
+
+            Console.WriteLine("Zadej mi jeden znak: ");
+            //Načte jeden znak do proměnne znak
+            char znak = Console.ReadKey().KeyChar;
+
+            //Podmínka, co mi řekne, jestli to je malé písmeno, či ne!
+            /* převod znaku na číslo
+            int cislo_znak = znak;
+            Console.WriteLine(cislo_znak);
+            */
+
+            //automaticky se znak převede na int
+            //int hodnota charu je daná ASCII tabulkou
+            //a=97
+            //z=122
+            //Malá abeceda je v číselném rozmezí 96 až 122
+            if(znak > 96 && znak < 123)
+            {
+                Console.WriteLine("Jedná se znak z malé abecedy!");
+            } else
+            {
+                Console.WriteLine("Nejedná se o znak z malé abecedy!");
+            }
+
+
+            //Program na jaké písmeno začíná zadané slovo
+            Console.WriteLine("Zadej mi slovo");
+            string word = Console.ReadLine();
+
+            if (word[0] > 96 && word[0] < 123)
+            {
+                Console.WriteLine("Slovo začíná malým písmenem!");
+            } else if (word[0] > 64 && word[0] < 91) //velká abeceda od 65 do 90
+            {
+                Console.WriteLine("Slovo začíná velkým písmenem!");
+            } else
+            {
+                Console.WriteLine("Slovo nezačíná písmenem, ale číslem nebo spec. znakem!");
+            }
+        }
+    }
+}
---
+++ 02_Vetveni/Switch/Switch/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Switch - den v týdnu
+            Console.WriteLine("Zadej mi pořadové číslo dne v týdnu.");
+            int den = int.Parse(Console.ReadLine());
+
+            //Switch vždy pracuje s hodnotou, případně více hodnotami
+            //Ty se zapisují do závorky switch(hodnota)
+            switch(den)
+            {
+                //case je jedna z možností, co může nastat
+                case 1:
+                    Console.WriteLine("Pondělí");
+                    break; //break (ukončení stávajícího příkazu) --> vyskočí to z switche ven
+                case 2:
+                    Console.WriteLine("Úterý");
+                    break;
+                case 3:
+                    Console.WriteLine("Středa");
+                    break;
+                case 4:
+                    Console.WriteLine("Čtvrtek");
+                    break;
+                case 5:
+                    Console.WriteLine("Pátek");
+                    break;
+            }
+
+            //Porovnávací operátory ve switchy
+            switch(den)
+            {
+                case 6:
+                    Console.WriteLine("Je víkend");
+                    break;
+                case 7:
+                    Console.WriteLine("Je víkend");
+                    break;
+                case < 0:
+                    Console.WriteLine("Neplatný vstup");
+                    break;
+                case > 7:
+                    Console.WriteLine("Neplatný vstup");
+                    break;
+                default: //spustí se pokud žádný case neodpovídá hodnotě
+                    Console.WriteLine("Je den v týdnu");
+                    break;
+            }
+
+            //switch --> posloupnost vyhodnocení
+            /*
+            switch(den)
+            {
+                case < 3:
+                    Console.WriteLine("Číslo je menší než tři");
+                    break;
+                case 2:
+                    Console.WriteLine("Číslo je dva!");
+                    break;
+                case < 0:
+                    Console.WriteLine("Číslo je záporné!");
+                    break;
+            }
+            */
+
+            //Porovnávací operátory a logické operátory ve switchy
+            switch (den)
+            {
+                //nahraje to hodnotu den do proměnné x
+                //následně to porovnává a vyhodnocuje podmínku za slovem WHEN
+                case int x when (x == 7 || x == 6): //x existuje pouze v rámci jednotlivého case
+                    Console.WriteLine("Je víkend");
+                    break;
+                case int x when (x < 1 || x > 7):
+                    Console.WriteLine("Neplatný vstup");
+                    break;
+                default: //spustí se pokud žádný case neodpovídá hodnotě
+                    Console.WriteLine("Je den v týdnu");
+                    break;
+            }
+          
+        }
+    }
+}
