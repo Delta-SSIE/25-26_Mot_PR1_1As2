784145c86693da905962870b438136a74382ad7e
Jan Mottl <mottja@delta-skola.cz>
2025-10-31 09:29:23 +0100

    kk

---
+++ 03_While/Soucet_v_intervalu/Soucet_v_intervalu/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Soucet_v_intervalu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zadej mi 1. číslo");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("zadej mi 2. číslo");
+            int y = int.Parse(Console.ReadLine());
+
+            //Zjištění max a min
+            int max = Math.Max(x, y);
+            int min = Math.Min(x, y);
+
+            int soucet = 0;
+
+            while (min <= max)
+            {
+                soucet += min;
+                min++;
+            }
+
+            Console.WriteLine($"Součet čísel v intervalu je {soucet}");
+        }
+    }
+}
---
+++ 03_While/Soucet_zadanych/Soucet_zadanych/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Soucet_zadanych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadávej čísla a já je budu sčítat. Vkládání ukončíš vložením nuly");
+            int soucet = 0;
+            int zad_cislo = 0;
+
+            do
+            {
+                Console.WriteLine("Zadej číslo");
+                zad_cislo = int.Parse(Console.ReadLine());
+                if (zad_cislo != 0)
+                {
+                    soucet += zad_cislo;
+                    Console.WriteLine($"Aktuální součet je {soucet}");
+                }
+            } while (zad_cislo != 0);
+        }
+    }
+}
