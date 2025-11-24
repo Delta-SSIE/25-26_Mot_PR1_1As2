721d63b3aad8bc7d976e38b49d8fe4e07589da14
Jan Mottl <mottja@delta-skola.cz>
2025-11-24 11:05:01 +0100

    kk

---
+++ 04_For/ObracenneSlovo/ObracenneSlovo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ObracenneSlovo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Ahoj, jak se máš?";
+            string text2 = "";
+
+            //Načtení znak po znaku z textu do textu2
+            for (int i = 0; i < text.Length; i++)
+            {
+                text2 += text[i];
+                Console.WriteLine(text2);
+            }
+
+            string obraceny_text = "";
+
+            //Načtení znak po znaku do obraceny_text odzadu textu
+            for (int i = text.Length-1; i >= 0; i--)
+            {
+                obraceny_text += text[i];
+                Console.WriteLine(obraceny_text);
+            }
+        }
+    }
+}
