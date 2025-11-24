c5eb0c082492448e2d1aa7981ac07160d0c1113e
Jan Mottl <mottja@delta-skola.cz>
2025-11-24 11:15:47 +0100

    kk

---
+++ 04_For/Proc_Vlozte_Oddelovac/Proc_Vlozte_Oddelovac/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Proc_Vlozte_Oddelovac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "skolaDelta";
+            char oddelovac = '/';
+
+            string text_novy = "";
+            // š/k/o/l/a/D/e/l/t/a
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(i == text.Length-1)
+                {
+                    text_novy += text[i];
+                } else
+                {
+                    text_novy += text[i] + "" + oddelovac;
+                    /*
+                     * text_novy += text[i];
+                     * text_novy += oddelovac;
+                     */
+                }
+
+                Console.WriteLine(text_novy);
+            }
+
+            Console.WriteLine(text_novy);
+        }
+    }
+}
