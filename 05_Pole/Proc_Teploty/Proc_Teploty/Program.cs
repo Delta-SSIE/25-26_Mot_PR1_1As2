42cb9236a7cd827e329f55e51c8036f8e0f74fd2
Jan Mottl <mottja@delta-skola.cz>
2025-12-08 11:16:37 +0100

    kk

---
+++ 05_Pole/Proc_Teploty/Proc_Teploty/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Proc_Teploty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] teploty = { 13.7, 14.2, 14.3, 14.8, 15.2, 16.0, 16.3, 17.1 };
+
+            //5:30 první měření
+            //každé další během 30 minut
+
+            int hodiny = 5;
+            int minuty = 30;
+
+            //Výpis
+            // 05:30 | 13,7°C
+
+            foreach (double teplota in teploty)
+            {
+                //cislo:00 znamená, že chci vypsat 2 čísla
+                Console.WriteLine($"{hodiny:00}:{minuty:00} | {teplota}°C");
+                minuty += 30;
+                if(minuty == 60)
+                {
+                    minuty = 0;
+                    hodiny++;
+                }
+
+            }
+        }
+    }
+}
