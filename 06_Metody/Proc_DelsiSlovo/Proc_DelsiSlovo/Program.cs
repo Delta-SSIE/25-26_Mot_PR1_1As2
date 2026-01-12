79d9109f99c3b6fb3ddfcb93c336c3c6205967af
Jan Mottl <mottja@delta-skola.cz>
2026-01-12 11:30:48 +0100

    kk

---
+++ 06_Metody/Proc_DelsiSlovo/Proc_DelsiSlovo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Proc_DelsiSlovo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej mi slovo A");
+            string slovoA = Console.ReadLine();
+            Console.WriteLine("Zadej mi slovo B");
+            string slovoB = Console.ReadLine();
+
+            Console.WriteLine($"Ze zadaných slov {slovoA} a {slovoB} je delší {JeDelsi(slovoA,slovoB)}");
+        }
+
+        static string JeDelsi(string a, string b)
+        {
+            if(a.Length > b.Length)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
