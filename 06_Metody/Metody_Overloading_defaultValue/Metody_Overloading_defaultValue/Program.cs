3a1ec65187a886785e416ba1413a5c691893e9c3
Jan Mottl <mottja@delta-skola.cz>
2026-01-16 08:42:19 +0100

    kk

---
+++ 06_Metody/Metody_Overloading_defaultValue/Metody_Overloading_defaultValue/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Metody_Overloading_defaultValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Scitani();
+            Scitani(1);
+            Scitani(5);
+
+            Nasobeni(5, 5); //zavolá Nasobeni(int x, int y)
+            Nasobeni(1.5, 2.5); //zavolá Nasobeni(double x, double y)
+
+            
+        }
+
+        //Method Overloading
+        //Dvě metody se stejný názvem, pracující s jiným dat. typem parametrů
+        //Program volá danou metodu, dle vstupních argumentů
+        static void Nasobeni (int x, int y)
+        {
+            Console.WriteLine(x*y);
+        }
+
+        static void Nasobeni(double x, double y)
+        {
+            Console.WriteLine(x*y);
+        }
+
+        //Metoda má výchozí hodnoty pro x a y, lze jí volat bez argumentů
+        static void Scitani(int x = 0, int y = 0)
+        {
+            Console.WriteLine(x+y);
+        }
+    }
+}
