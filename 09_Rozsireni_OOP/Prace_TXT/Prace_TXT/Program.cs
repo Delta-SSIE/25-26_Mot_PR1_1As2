0ce43e5d4db44938c9d3d6103cd60123b0e07856
Jan Mottl <mottja@delta-skola.cz>
2026-05-04 12:58:06 +0200

    kk

---
+++ 09_Rozsireni_OOP/Prace_TXT/Prace_TXT/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Prace_TXT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //WRITEALLTEXT a READALLTEXT
+            string txt = "Tenhle text se zapíše do souboru!";
+
+            //Zapíše obsah proměnné txt do text.txt, pokud by tam něco bylo, tak to přepíše
+            //projektová složka -> bin -> debug -> net 9.0
+            File.WriteAllText("text.txt", txt);
+
+            //Načte text jako jeden string
+            Console.WriteLine(File.ReadAllText("text.txt"));
+
+            //WRITEALLLINES a READALLLINES
+
+            string[] poleTxt = ["První řádek", "Druhý řádek", "Třetí řádek"];
+            //Zapíše data z kolekce jako jednotlivé řádky 
+            File.WriteAllLines("text2.txt", poleTxt);
+
+            string[] vystup = File.ReadAllLines("text2.txt");
+            foreach(string radek in vystup)
+            {
+                Console.WriteLine(radek);
+            }
+
+            //APPEND - přidává do souboru
+            File.AppendAllText("text.txt", "Ahooooj");
+            File.AppendAllLines("text2.txt", vystup);
+            File.AppendAllText("text3.txt", "DATA");
+        }
+    }
+}
---
+++ 09_Rozsireni_OOP/StreamReader_StreamWriter/StreamReader_StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StreamReader_StreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cestaLogu = "zaznamy.txt";
+
+            // Druhý parametr 'true' znamená, že text se bude na konec souboru PŘIDÁVAT (nepřepíše ho)
+            using (StreamWriter sw = new StreamWriter(cestaLogu, true))
+            {
+                sw.WriteLine("Začátek nového záznamu");
+
+                for (int i = 1; i <= 5; i++)
+                {
+                    sw.WriteLine($"Záznam číslo: {i}");
+                }
+            } // Zde se StreamWriter automaticky zavře
+
+            using (StreamReader sr = new StreamReader(cestaLogu))
+            {
+                string radek;
+
+                // Čteme postupně, dokud metoda ReadLine nevrátí null
+                while ((radek = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(radek);
+                }
+            }
+        }
+    }
+}
